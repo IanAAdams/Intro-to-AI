c51cb4487174f2e914ba6256fad46b9a4c4e6f63
IanAAdams <142043517+IanAAdams@users.noreply.github.com>
2025-11-11 14:28:20 -0500

    Add files via upload

--- Neural-Network.R
+++ Neural-Network.R
@@ -12,3 +12,51 @@ validation <- dataset[-validation_index,]
 dataset <- dataset[validation_index,]
 
 
+# cross validations: 3, 5, 10-fold cross validations 
+control <- trainControl(method="cv", number=10) 
+metric <- "Accuracy"
+
+set.seed(7) 
+fit.lda <- train(Species~., data=dataset, method="lda", metric=metric, trControl= control)
+
+set.seed(7) 
+fit.rf <- train(Species~., data=dataset, method="rf", metric=metric, trControl= control)
+
+# Summarizing the accuracy of the LDA  models
+results <- resamples(list(lda=fit.lda, rf=fit.rf)) 
+summary(results)
+
+# Based upon the accuracy values we choose lda model to do our predictions on the validation dataset 
+predictions <- predict(fit.lda, validation) 
+confusionMatrix(predictions, validation$Species)
+
+#Confusion Matrix and Statistics
+
+#            Reference
+#Prediction   setosa versicolor virginica
+# setosa         10          0         0
+#  versicolor      0         10         0
+ # virginica       0          0        10
+
+#Overall Statistics
+                                     
+ #              Accuracy : 1          
+  #               95% CI : (0.8843, 1)
+   # No Information Rate : 0.3333     
+   # P-Value [Acc > NIR] : 4.857e-15  
+                                     
+    #              Kappa : 1          
+                                     
+# Mcnemar's Test P-Value : NA         
+
+#Statistics by Class:
+
+  #                   Class: setosa Class: versicolor Class: virginica
+#Sensitivity                 1.0000            1.0000           1.0000
+#Specificity                 1.0000            1.0000           1.0000
+#Pos Pred Value              1.0000            1.0000           1.0000
+#Neg Pred Value              1.0000            1.0000           1.0000
+#Prevalence                  0.3333            0.3333           0.3333
+#Detection Rate              0.3333            0.3333           0.3333
+#Detection Prevalence        0.3333            0.3333           0.3333
+#Balanced Accuracy           1.0000            1.0000           1.0000
